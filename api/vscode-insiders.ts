2c2322c8b90a0d7f7ddf3e46741511b7c5e8b90f
Kid <kidonng@gmail.com>
2020-07-22 12:37:12 +0800

    Use commit date as version for `vscode-insiders`
    Scoop (and possibly any package manager) isn't that
    smart to figure out whether a commit is newer

--- api/vscode-insiders.ts
+++ api/vscode-insiders.ts
@@ -1,17 +1,23 @@
 import got from 'got'
 import { NowRequest, NowResponse } from '@vercel/node'
+import dayjs from 'dayjs'
 
 export default ({ query: { x64 } }: NowRequest, { send }: NowResponse) => {
     got(
         `https://update.code.visualstudio.com/latest/win32-${
             x64 ? 'x64-' : ''
         }archive/insider`
-    ).on('response', (response) => {
+    ).on('response', async (response) => {
         const { pathname } = new URL(response.redirectUrls[0])
-        const split = pathname.split('/')
-        const version = split[3].match(/([\d.]+)-insider/)[1]
-        const commit = split[2]
+        const [, , commit, filename] = pathname.split('/')
+        const [, version] = filename.match(/([\d.]+)-insider/)
 
-        send(`${version} ${commit}`)
+        const {
+            author: { date },
+        } = await got(
+            `https://api.github.com/repos/microsoft/vscode/git/commits/${commit}`
+        ).json()
+
+        send(`${version}-${dayjs(date).format('YYYYMMDDHHss')}`)
     })
 }
